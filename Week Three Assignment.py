c88de400e758bac4af13903f6143dbfc84c1ca6a
Cedrick <cedricshik@gmail.com>
2025-03-28 17:30:08 +0300

    Week Three Assignment Submission

--- Week Three Assignment.py
+++ Week Three Assignment.py
@@ -1,1 +1,18 @@
+# Week Three Assignment
+  
+# Question  1  
+    
+def calculate_discount(price, discount_percent):
+    if discount_percent >= 20:
+        discount_amount = price * (discount_percent / 100)
+        total_price = price - discount_amount
+        return total_price
+    else:
+        return price
+
+# Question 2
+price = float(input("Enter the item price: "))
+discount_percent = float(input("Enter the discount percentage: "))
+total_price = calculate_discount(price, discount_percent)
+print("The discounted total price is: ", total_price)
 
